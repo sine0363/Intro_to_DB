76d09f045fef41c387d9aa2078f197609f0372a6
Sinethemba Zulu <sinethemba0363@gmail.com>
2025-10-19 13:16:40 +0200

    fg

---
+++ task_4.sql
@@ -0,0 +1,14 @@
+-- task_4.sql
+-- Print full description of the table 'books'
+
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Data Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE()
+  AND TABLE_NAME = 'books'
+ORDER BY ORDINAL_POSITION;
